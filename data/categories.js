387b550d4790eb2eacf4f37235a32292c2c147fe
mclaramarinho <marinho.claramb@gmail.com>
2024-05-28 19:50:28 -0300

    FEAT: created categories data file

---
+++ data/categories.js
@@ -0,0 +1,33 @@
+export const categories = [
+    {
+        name: "Jantar",
+        description: "Comidas de jantar",
+        href: "catalog?category=jantar"
+    },
+    {
+        name: "Almoço",
+        description: "Comidas de almoço",
+        href: "catalog?category=almoco"
+    },
+    {
+        name: "Sobremesas",
+        description: "Comidas de jantar",
+        href: "catalog?category=sobremesas"
+    },
+    {
+        name: "Pratos Regionais",
+        description: "Comidas de jantar",
+        href: "catalog?category=regional"
+    },
+    ,
+    {
+        name: "Flex",
+        description: "Comidas de flex",
+        href: "catalog?category=sobremesas"
+    },
+    {
+        name: "Vegano",
+        description: "Comidas de vegan",
+        href: "catalog?category=regional"
+    },
+]
