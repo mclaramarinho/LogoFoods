61d99902e593ccffe05cfbefabd9d81500b24a9f
mclaramarinho <marinho.claramb@gmail.com>
2024-05-28 21:13:27 -0300

    FIX: added href attribute to products

--- data/products.js
+++ data/products.js
@@ -10,43 +10,50 @@ export const products = [
         prodTitle: "Chinchila 7",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     {
         prodTitle: "Chinchila 6",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     {
         prodTitle: "Chinchila 5",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     {
         prodTitle: "Chinchila 4",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     {
         prodTitle: "Chinchila 3",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     {
         prodTitle: "Chinchila 2",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     {
         prodTitle: "Chinchila 1",
         prodDesc: "5 Marmitas de almooço regional do seu local preferido",
         prodPrice: 90.99,
-        imgSrc: "./assets/spiky.jpg"
+        imgSrc: "./assets/spiky.jpg",
+        href: ""
     },
     
 ];
