3f83919c2e3d39c68fd4e4d0718c7bc350666faa
mclaramarinho <marinho.claramb@gmail.com>
2024-06-08 13:08:18 -0300

    FIX: updated categories list to have imgSrc attribute

--- data/categories.js
+++ data/categories.js
@@ -2,32 +2,38 @@ export const categories = [
     {
         name: "Jantar",
         description: "Comidas de jantar",
-        href: "catalog?category=jantar"
+        href: "catalog?category=jantar",
+        imgSrc:"./assets/dinnerCategory.png"
     },
     {
         name: "Almoço",
         description: "Comidas de almoço",
-        href: "catalog?category=almoco"
+        href: "catalog?category=almoco",
+        imgSrc:"./assets/lunchCategory.png"
     },
     {
         name: "Sobremesas",
         description: "Comidas de jantar",
-        href: "catalog?category=sobremesas"
+        href: "catalog?category=sobremesas",
+        imgSrc:"./assets/snacksCategory.png"
     },
     {
         name: "Pratos Regionais",
         description: "Comidas de jantar",
-        href: "catalog?category=regional"
+        href: "catalog?category=regional",
+        imgSrc:"./assets/regionalCategory.png"
     },
     ,
     {
         name: "Flex",
         description: "Comidas de flex",
-        href: "catalog?category=sobremesas"
+        href: "catalog?category=sobremesas",
+        imgSrc:"./assets/flexCategory.jpg"
     },
     {
         name: "Vegano",
         description: "Comidas de vegan",
-        href: "catalog?category=regional"
+        href: "catalog?category=regional",
+        imgSrc:"./assets/veganCategory.png"
     },
 ]
