3bb9e8b763d582945081c3d7dc2de6d43714edd2
mclaramarinho <marinho.claramb@gmail.com>
2024-06-10 19:41:27 -0300

    FEAT: created dictionary for identifying products' characteristics

---
+++ data/productCharacteristics.js
@@ -0,0 +1,26 @@
+export const productCharacteristics = {
+    "frozen": {
+        icon: "fa fa-snowflake"
+    },
+    "zero-lactose":{
+        icon: "mdi mdi-cow-off"
+    },
+    "seafood":{
+        icon: "fa fa-shrimp"
+    },
+    "meal": {
+        icon: "mdi mdi-noodles"
+    },
+    "snack": {
+        icon: "fa fa-pizza-slice"
+    },
+    "dessert": {
+        icon: "fa fa-ice-cream"
+    },
+    "peanut": {
+        icon: "mdi mdi-peanut"
+    },
+    "vegan": {
+        icon: "mdi mdi-food-drumstick-off"
+    }
+}
