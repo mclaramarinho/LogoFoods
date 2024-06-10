a7a60fda5c001bf8ee11e7d405e75d600ce83e56
mclaramarinho <marinho.claramb@gmail.com>
2024-06-10 19:35:30 -0300

    FEAT: implemented routes to application

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+import Footer from "./components/Footer.js";
+import Navbar from "./components/Navbar.js";
+import SearchFilter from "./components/SearchFilter.js";
+import { footerData } from "./data/footerData.js";
+import CatalogPage from "./pages/CatalogPage.js";
 import "./pages/HomePage.js";
+import HomePage from "./pages/HomePage.js";
+
+
+const params = window.location.search;
+
+// path.length === 0 --> home
+// ?catalog --> catalog 
+// ?catalog=&category --> catalog filtered by categories
+// ?catalog=&productName --> catalog filtered by product name
+// ?catalog=&category=&productName --> catalog filtered by categories and productName
+// 
+
+
+
+new Navbar();
+
+
+if(params.length === 0){
+    new HomePage()
+}else if(params.includes("catalog")){
+    new CatalogPage();
+}
+
+
+new Footer(footerData);
