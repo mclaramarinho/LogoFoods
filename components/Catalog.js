b2439edb9215c0ba79a59dbbd86a3779441f99d5
mclaramarinho <marinho.claramb@gmail.com>
2024-06-10 19:36:42 -0300

    FEAT: created catalog component

---
+++ components/Catalog.js
@@ -0,0 +1,37 @@
+import FilteredProducts from "../data/FilteredProducts.js";
+import CatalogCard from "./CatalogCard.js";
+
+export default class Catalog{
+    elementToAttach = document.getElementById("main");
+    container = document.createElement("div");
+    catalogHeaderText = "";
+
+    constructor(elementToAttach=null){
+        if(elementToAttach !== null) {
+            this.elementToAttach = document.getElementById(elementToAttach);
+        }
+
+        this.container.classList.add("catalog_container");
+        this.container.id = "catalog_container";
+        
+        const filteredProds = new FilteredProducts();
+
+        this.catalogHeaderText = "Mostrando todos os produtos";
+        if(filteredProds.productName !== null){
+            this.catalogHeaderText = `Mostrando produtos com o nome ${filteredProds.productName}`;
+        }else{
+            if(filteredProds.hasFiltersApplied){
+                this.catalogHeaderText = "Mostrando todos os resultados para os filtros aplicados";
+            }
+        }
+
+        this.elementToAttach.innerHTML = `<h2 class="font-red">${this.catalogHeaderText}</h2>`;
+        
+        this.elementToAttach.append(this.container);
+        
+
+        filteredProds.list.forEach(item => {
+            new CatalogCard(this.container.id, item);
+        });
+    }
+}
