3768ea5a6035e89eca7873fefc934ce65b9e40d8
mclaramarinho <marinho.claramb@gmail.com>
2024-06-10 19:40:29 -0300

    FEAT: added tag attribute to category object

--- data/categories.js
+++ data/categories.js
@@ -1,24 +1,35 @@
 export const categories = [
     {
         name: "Jantar",
+        tag: "dinner",
         description: "Comidas de jantar",
         href: "catalog?category=jantar",
         imgSrc:"./assets/dinnerCategory.png"
     },
     {
         name: "Almoço",
+        tag: "lunch",
         description: "Comidas de almoço",
         href: "catalog?category=almoco",
         imgSrc:"./assets/lunchCategory.png"
     },
     {
         name: "Sobremesas",
+        tag: "desserts",
         description: "Comidas de jantar",
         href: "catalog?category=sobremesas",
         imgSrc:"./assets/snacksCategory.png"
     },
     {
+        name: "Lanches",
+        tag: "snacks",
+        description: "Lanches",
+        href: "catalog?category=snacks",
+        imgSrc:"./assets/snacksCategory.png"
+    },
+    {
         name: "Pratos Regionais",
+        tag: "regional",
         description: "Comidas de jantar",
         href: "catalog?category=regional",
         imgSrc:"./assets/regionalCategory.png"
@@ -26,12 +37,14 @@ export const categories = [
     ,
     {
         name: "Flex",
+        tag: "flex",
         description: "Comidas de flex",
         href: "catalog?category=sobremesas",
         imgSrc:"./assets/flexCategory.jpg"
     },
     {
         name: "Vegano",
+        tag: "vegan",
         description: "Comidas de vegan",
         href: "catalog?category=regional",
         imgSrc:"./assets/veganCategory.png"
