2e87ed2775cc1884786ef425345720877c0981c3
mclaramarinho <marinho.claramb@gmail.com>
2024-06-10 19:42:42 -0300

    FIX: updated homepage to be a class + added start section

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -6,22 +6,30 @@ import { products } from "../data/products.js";
 import { categories } from "../data/categories.js";
 import Footer from "../components/Footer.js";
 import { footerData } from "../data/footerData.js";
-new Navbar();
 
-// SECTION 1
-const superDescontosHeader = new SectionHeader("SUPER DESCONTOS").getHtmlElement();
-new Section([superDescontosHeader]);
-const carousel = new Carousel(products, ["mb-5"]);
 
-// SECTION 2
-const superDescontosHeader2 = new SectionHeader("EM ALTA").getHtmlElement();
-new Section([superDescontosHeader2]);
-const carousel2 = new Carousel(products, ["mb-5"], false, true);
 
-// SECTION 3
-const categoriesHeader = new SectionHeader("Pesquisar por categoria").getHtmlElement();
-new Section([categoriesHeader]);
-const carousel3 = new Carousel(categories, ["mb-5"], true);
+export default class HomePage{
 
+    
+    // START SECTION
+    headerSection = new Section([], true);
+
+
+    // SECTION 1
+    superDescontosHeader = new SectionHeader("SUPER DESCONTOS").getHtmlElement();
+    superDescontosCarousel = new Carousel(products, ["mb-5"], false, false);
+    superDescontosSection = new Section([this.superDescontosHeader, this.superDescontosCarousel.container]);
+
+    // SECTION 2
+    emAltaHeader = new SectionHeader("EM ALTA").getHtmlElement();
+    emAltaSection = new Section([this.emAltaHeader]);
+    emAltaCarousel = new Carousel(products, ["mb-5"], false, true);
+
+    // SECTION 3
+    categoriesHeader = new SectionHeader("Pesquisar por categoria").getHtmlElement();
+    categoriesSection = new Section([this.categoriesHeader]);
+    categoriesCarousel = new Carousel(categories, ["mb-5"], true);
+}
 // FOOTER
-new Footer(footerData);
+// new Footer(footerData);
