6bf3a6ec433a5f9cd6c263161dcb2490bcfcb038
mclaramarinho <marinho.claramb@gmail.com>
2024-06-13 22:10:52 -0300

    FEAT: applies mask to all masked inputs

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import Footer from "./components/Footer.js";
 import Navbar from "./components/Navbar.js";
-import SearchFilter from "./components/SearchFilter.js";
 import { footerData } from "./data/footerData.js";
-import CatalogPage from "./pages/CatalogPage.js";
-import DetailsPage from "./pages/DetailsPage.js";
 import "./pages/HomePage.js";
-import HomePage from "./pages/HomePage.js";
-import { User, users } from "./data/users.js";
 import Cart from "./localstorage/cart.js"
 import Router from "./router.js";
 import Auth from "./localstorage/auth.js";
+import applyMask from "./utils/mask.js";
 
 export const cart = new Cart();
 export const auth = new Auth();
-
 // auth.login("clara@email.com", "password")
 
 export const navbar = new Navbar();
@@ -21,3 +16,5 @@ export const navbar = new Navbar();
 new Router();
 
 new Footer(footerData);
+
+applyMask();
